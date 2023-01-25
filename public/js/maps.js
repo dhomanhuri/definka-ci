9b241bd3959714657928081b898c981cbb48bd52
dhomanhurimtm <dhomanhuri@mtm.id>
2023-01-25 16:25:50 +0700

    change port

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,43 +1,45 @@
-( function ( $ ) {
+(function ($) {
+    var charts = {
+        init: function () {
+            this.ajaxPostMap();
+        },
 
-	var charts = {
-		init: function () {
-			this.ajaxPostMap();
-		},
+        ajaxPostMap: function () {
+            var urlPath =
+                window.location.protocol +
+                "//" +
+                window.location.hostname +
+                ":1881/post-maps";
+            var request = $.ajax({
+                method: "GET",
+                url: urlPath,
+            });
 
-		ajaxPostMap: function () {
-			var urlPath =  window.location.protocol + '//' + window.location.hostname + '/post-maps';
-			var request = $.ajax( {
-				method: 'GET',
-				url: urlPath
-		} );
+            request.done(function (response) {
+                charts.createCompletedJobsMap(response);
+            });
+        },
 
-			request.done( function ( response ) {
-				charts.createCompletedJobsMap( response );
-			});
-		},
+        createCompletedJobsMap: function (response) {
+            const data = response.data;
+            data.forEach(push);
 
-		createCompletedJobsMap: function ( response ) {
-			const data = response.data;
-			data.forEach(push);
-			
-			function push(item)
-			{
-				var title = document.getElementById(item.place);
-				var path = document.querySelector('[title="' + item.place + '"]');
+            function push(item) {
+                var title = document.getElementById(item.place);
+                var path = document.querySelector(
+                    '[title="' + item.place + '"]'
+                );
 
-				title.innerHTML = 'Jumlah Alumni: ' + item.count + '\nDaerah: ' + item.place
-				if (item.count < 5) {
-					path.classList.add('med-color');
-				}
-				else {
-					path.classList.add('high-color');
-				}
-			}
+                title.innerHTML =
+                    "Jumlah Alumni: " + item.count + "\nDaerah: " + item.place;
+                if (item.count < 5) {
+                    path.classList.add("med-color");
+                } else {
+                    path.classList.add("high-color");
+                }
+            }
+        },
+    };
 
-		}
-	};
-
-	charts.init();
-
-} )( jQuery );
+    charts.init();
+})(jQuery);
